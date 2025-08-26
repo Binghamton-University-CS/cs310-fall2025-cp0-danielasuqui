5a51a53d876acd3191a48a86de3982dbe8d40926
esuquiestrel <esuquiestrel@cs.binghamton.edu>
2025-08-26 10:40:27 -0400

    read me updated

--- zoo.cpp
+++ zoo.cpp
@@ -23,12 +23,13 @@ class Animal {
 int main() {
    Animal *animal1 = new Animal("African Elephant", 1758);
    Animal animal2("Giant Panda", 1869);
+   Animal animal3("Clouded Tiger Cat", 2024);
 
    delete animal1;
    animal1 = new Animal("Snow Leopard", 1777);
 
    animal2.display();
    animal1->display();
-
+   animal3.display();
    delete animal1;
 }
